87f23ed7bb074e89f35f2065fb8d722b615023be
Uri Berger <uriber@cs.huji.ac.il>
2023-08-09 14:16:03 +0300

    Changed collection of image ids to be unique

--- reformulation_experiment/prepare_base_training_data.py
+++ reformulation_experiment/prepare_base_training_data.py
@@ -9,7 +9,7 @@ base_train_sample_num = int(sys.argv[2])
 
 with open('/cs/labs/oabend/uriber/datasets/STAIR-captions/stair_captions_v1.2_train.json', 'r') as fp:
     train_data = json.load(fp)['annotations']
-train_image_ids = [x['image_id'] for x in train_data]
+train_image_ids = list(set([x['image_id'] for x in train_data]))
 base_image_ids = random.sample(train_image_ids, base_train_sample_num)
 
 base_image_ids_dict = {x: True for x in base_image_ids}
